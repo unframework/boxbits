d6e34b388cb4a5465ff799eef347ac8785b94c26
Nick Matantsev <nick.matantsev@gmail.com>
2015-03-03 00:07:14 -0500

    Gutting old deps

--- preview/example.js
+++ preview/example.js
@@ -1,16 +1,16 @@
 
 b = require('boxbits');
 
-b.display(
-    b.layers([
-        b.west(topMenu = b.box()),
-        b.center(topBarLogo = b.icon()),
-        b.east(
-            b.row([
-                searchBox = b.box(),
-                profileIcon = b.box(),
-                searchBox = b.box()
-            ])
-        )
-    ])
-)
+// b.display(
+//     b.layers([
+//         b.west(topMenu = b.box()),
+//         b.center(topBarLogo = b.icon()),
+//         b.east(
+//             b.row([
+//                 searchBox = b.box(),
+//                 profileIcon = b.box(),
+//                 searchBox = b.box()
+//             ])
+//         )
+//     ])
+// )
