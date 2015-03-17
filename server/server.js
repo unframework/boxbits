aa45172d01cd6e90243e9f0cf15f084c77258b44
Nick Matantsev <nick.matantsev@gmail.com>
2015-03-16 23:57:34 -0400

    Removed JS caching

--- server/server.js
+++ server/server.js
@@ -6,19 +6,16 @@ var concat = require('concat-stream')
 var baseHtml = '<!DOCTYPE html><html><body><div id="container"></div><script src="main.js"></script></body></html>';
 
 module.exports = function (jsFile) {
-    var jsCache;
-
     b.add(jsFile);
-    b.bundle().pipe(concat(function(js) {
-        jsCache = js;
-    }));
 
     var server = http.createServer(function(req, res){
         var done = finalhandler(req, res)
 
         if (req.url === '/main.js') {
             res.setHeader('Content-Type', 'application/javascript');
-            res.end(jsCache.toString());
+            b.bundle().pipe(concat(function(js) {
+                res.end(js.toString());
+            }));
         } else if (req.url === '/') {
             res.setHeader('Content-Type', 'text/html');
             res.end(baseHtml);
