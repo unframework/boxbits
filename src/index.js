f29633f193d18aba50b3ea80936eaad65a9c71b6
Nick Matantsev <nick.matantsev@gmail.com>
2015-07-03 14:29:02 -0400

    Fixed whitespace

--- src/index.js
+++ src/index.js
@@ -12,149 +12,149 @@ var createElement = require('virtual-dom/create-element');
 
 // @todo reconsider how this is done
 document.getElementsByTagName('head')[0].appendChild((function (html) {
-  var span = document.createElement('span');
-  span.innerHTML = html;
-  return span.firstChild;
+    var span = document.createElement('span');
+    span.innerHTML = html;
+    return span.firstChild;
 })('<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300" type="text/css" />'));
 
 module.exports = {
-  column: function() {
-    var specList = Array.prototype.slice.call(arguments, 0);
+    column: function() {
+        var specList = Array.prototype.slice.call(arguments, 0);
 
-    return h('div', {
-      style: {
-        flex: 1,
-        display: 'flex',
-        justifyContent: 'space-around', // automatic centering when just one item
-        flexDirection: 'column'
-      }
-    }, specList);
-  },
+        return h('div', {
+            style: {
+                flex: 1,
+                display: 'flex',
+                justifyContent: 'space-around', // automatic centering when just one item
+                flexDirection: 'column'
+            }
+        }, specList);
+    },
 
-  row: function() {
-    var specList = Array.prototype.slice.call(arguments, 0);
+    row: function() {
+        var specList = Array.prototype.slice.call(arguments, 0);
 
-    return h('div', {
-      style: {
-        flex: 1,
-        display: 'flex',
-        justifyContent: 'space-around', // automatic centering when just one item
-        flexDirection: 'row'
-      }
-    }, specList);
-  },
+        return h('div', {
+            style: {
+                flex: 1,
+                display: 'flex',
+                justifyContent: 'space-around', // automatic centering when just one item
+                flexDirection: 'row'
+            }
+        }, specList);
+    },
 
-  spacer: function(vdom) {
-    return h('div', {
-      style: {
-        flex: 1
-      }
-    }, vdom);
-  },
+    spacer: function(vdom) {
+        return h('div', {
+            style: {
+                flex: 1
+            }
+        }, vdom);
+    },
 
-  box: function(opts, content) {
-    return h('div', {
-      style: {
-        margin: opts.expand ? 0 : 'auto', // center or expand in flex
-        padding: (opts.padding || 0) * 1 + 'px',
-        borderRadius: (opts.borderRadius || 0) * 1 + 'px',
-        backgroundColor: opts.backgroundColor || 'transparent',
-        display: 'flex',
-        flexDirection: 'inherit' // @todo this just seems like an intuitive value
-      }
-    }, content);
-  },
+    box: function(opts, content) {
+        return h('div', {
+            style: {
+                margin: opts.expand ? 0 : 'auto', // center or expand in flex
+                padding: (opts.padding || 0) * 1 + 'px',
+                borderRadius: (opts.borderRadius || 0) * 1 + 'px',
+                backgroundColor: opts.backgroundColor || 'transparent',
+                display: 'flex',
+                flexDirection: 'inherit' // @todo this just seems like an intuitive value
+            }
+        }, content);
+    },
 
-  button: function() {
-    var contents = Array.prototype.slice.call(arguments, 0);
-    var handler = contents.pop();
+    button: function() {
+        var contents = Array.prototype.slice.call(arguments, 0);
+        var handler = contents.pop();
 
-    return h('button', {
-      onclick: handler,
-      style: {
-        margin: 0,
-        padding: 0,
-        flex: 1,
-        display: 'flex',
-        justifyContent: 'space-around', // automatic centering when just one item
-        flexDirection: 'row'
-      }
-    }, contents);
-  },
+        return h('button', {
+            onclick: handler,
+            style: {
+                margin: 0,
+                padding: 0,
+                flex: 1,
+                display: 'flex',
+                justifyContent: 'space-around', // automatic centering when just one item
+                flexDirection: 'row'
+            }
+        }, contents);
+    },
 
-  verticalScroll: function(content) {
-    return h('div', {
-      style: {
-        flex: 1,
-        overflowX: 'hidden',
-        overflowY: 'scroll'
-      }
-    }, h('div', {
-      style: {
-        display: 'flex',
-        flexDirection: 'column'
-      }
-    }, content));
-  },
+    verticalScroll: function(content) {
+        return h('div', {
+            style: {
+                flex: 1,
+                overflowX: 'hidden',
+                overflowY: 'scroll'
+            }
+        }, h('div', {
+            style: {
+                display: 'flex',
+                flexDirection: 'column'
+            }
+        }, content));
+    },
 
-  list: function(opts) {
-    var specList =  Array.prototype.slice.call(arguments, 1);
-    var separator = (opts.spacing || 0) + 'px';
+    list: function(opts) {
+        var specList =  Array.prototype.slice.call(arguments, 1);
+        var separator = (opts.spacing || 0) + 'px';
 
-    return h('div', {
-      style: {
-        flex: 1,
-        display: 'flex',
-        justifyContent: 'flex-start',
-        flexDirection: 'inherit' // @todo more explicit?
-      }
-    }, specList.map(function(spec, i) {
-      return [
-        i === 0 ? null : h('div', {
-          style: {
-            flexGrow: 0,
-            flexShrink: 0,
-            flexBasis: separator
-          }
-        }),
-        spec
-      ];
-    }));
-  },
+        return h('div', {
+            style: {
+                flex: 1,
+                display: 'flex',
+                justifyContent: 'flex-start',
+                flexDirection: 'inherit' // @todo more explicit?
+            }
+        }, specList.map(function(spec, i) {
+            return [
+                i === 0 ? null : h('div', {
+                    style: {
+                        flexGrow: 0,
+                        flexShrink: 0,
+                        flexBasis: separator
+                    }
+                }),
+                spec
+            ];
+        }));
+    },
 
-  text: function(content) {
-    return h('div', {
-      style: {
-        flexGrow: 0,
-        flexShrink: 0,
-        margin: 'auto', // center in flex
-        fontFamily: 'Open Sans',
-        fontWeight: 300,
-        fontSize: '16px',
-        lineHeight: '16px',
-        whiteSpace: 'nowrap',
-        width: 'auto',
-        height: '18px', // compensate for ascenders
-        overflow: 'hidden'
-      }
-    }, '' + content);
-  },
+    text: function(content) {
+        return h('div', {
+            style: {
+                flexGrow: 0,
+                flexShrink: 0,
+                margin: 'auto', // center in flex
+                fontFamily: 'Open Sans',
+                fontWeight: 300,
+                fontSize: '16px',
+                lineHeight: '16px',
+                whiteSpace: 'nowrap',
+                width: 'auto',
+                height: '18px', // compensate for ascenders
+                overflow: 'hidden'
+            }
+        }, '' + content);
+    },
 
-  icon: function(width, height, imageData) {
-    return h('div', {
-      style: {
-        flexGrow: 0,
-        flexShrink: 0,
-        background: 'url(data:application/octet-stream;base64,' + imageData + ')',
-        backgroundSize: 1 * width + 'px ' + 1 * height + 'px',
-        margin: 'auto',
-        width: width + 'px',
-        height: height + 'px'
-      }
-    });
-  },
+    icon: function(width, height, imageData) {
+        return h('div', {
+            style: {
+                flexGrow: 0,
+                flexShrink: 0,
+                background: 'url(data:application/octet-stream;base64,' + imageData + ')',
+                backgroundSize: 1 * width + 'px ' + 1 * height + 'px',
+                margin: 'auto',
+                width: width + 'px',
+                height: height + 'px'
+            }
+        });
+    },
 
-  display: function(vdom) {
-    return document.body.appendChild(createElement(vdom));
-  }
+    display: function(vdom) {
+        return document.body.appendChild(createElement(vdom));
+    }
 };
